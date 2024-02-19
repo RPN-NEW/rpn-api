f4f0719159f3235e29e4cee3f76869890bf0bfa4
RanopRamah <101007427+RanopRamah@users.noreply.github.com>
2024-02-19 16:08:32 +0700

    Create, Get Article

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function createArticle(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'title' => 'required|max:1000',
+                'description' => 'required',
+                'author' => 'max:200'
+            ]);
+
+            $article = new Article();
+            $article->title = $request->input('title');
+            $article->description = $request->input('description');
+            $article->author = $request->input('author');
+            $article->save();
+
+            return response()->json(['message' => 'Successfully article created', 'article' => $article], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to create article' . $e->getMessage()], 404);
+        }
+    }
+
+    public function showArticle()
+    {
+        $article = Article::all();
+        return response()->json(['article' => $article], 200);
+
+        if ($article->isEmpty()) {
+            return response()->json(['message' => 'Belum ada artikel']);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/create-article', [AdminController::class, 'createArticle']);
+Route::get('/article', [AdminController::class, 'showArticle']);
